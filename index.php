5cdd35fb7dacc816e6e3ae3d8a668c6f8c076c65
Muhammad Tariq <mtariq_sarwar@yahoo.com>
2013-09-02 14:23:14 +0500

    pairness index file

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/*********************************************
+
+	Copyright:	Cozmuler Pakistan
+	File Name:	index.php
+	Package:	Pairness.com
+	Author:		Rahber
+
+*********************************************/
+include('./includes/functions.php');
+include('./assets/template/header.coz');
+include('./assets/template/main.coz');
+include('./assets/template/footer.coz');
+
+
+?>
